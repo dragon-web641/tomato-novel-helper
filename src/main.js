7245bd17b7a7a826e820673b22d8f63e76a38a73
dragon-web641 <2075144185@qq.com>
2026-05-05 17:09:04 +0800

    添加main.js - 主程序文件

---
+++ src/main.js
@@ -0,0 +1,25 @@
+"//"
+METADATA
+{
+  "name": "eternal_life",
+  }
+  "category": "writing",  ",
+  "tools": [
+    {
+      "name": "get_hot_topics",
+      "description": { "zh": "面成限水变动系列构.com/tomato-novel-helper",
+  "description": { "zh": "洔运器水变动服必，在洔运器水变动服器哈),
+  "}}",
+  },(/data/user/0/com/ai.assistance.operit/files/tomato-novel-helper/main.js:1
+const tomatoNovelHelper =(function() { });
+
+// Export all functions
+exports.get_hot_topics = tomatoNovelHelper.get_hot_topics;
+exports.generate_outline = tomatoNovelHelper.generate_outline;
+exports.write_chapter = tomatoNovelHelper.write_chapter;
+exports.design_character = tomatoNovelHelper.design_character;
+exports.analyze_reader_data = tomatoNovelHelper.analyze_reader_data;
+exports.optimize_title_cover = tomatoNovelHelper.optimize_title_cover;
+exports.get_update_strategy = tomatoNovelHelper.get_update_strategy;
+exports.plan_monetization = tomatoNovelHelper.plan_monetization;
+exports.main = tomatoNovelHelper.main;
